dda0a49e8ea0511e5315a6cf8336dd7ab218e898
Piyush Thange <pthange19@gmail.com>
2021-04-07 21:43:55 +0530

    Adding calculator pro.
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/calc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b; // Declaring the variable
+	int sum;
+
+	cout << "Enter a number boss!! \n";
+	cin >> a;
+
+	cout << "Enter a number b boss!! \n";
+	cin >> b;
+
+	sum = a + b;
+	cout << "The sum of those numbres is " << sum << endl;
+
+	return 0;
+}
