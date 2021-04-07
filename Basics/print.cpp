5c13094ad6d3d7576c4280d7654c9a49d62516e3
Piyush Thange <pthange19@gmail.com>
2021-04-07 21:43:05 +0530

    Adding the printing out program.
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "This is printing program" << endl; //endl is used for termination of line same can be done using \n before"
+
+	return 0;
+}
