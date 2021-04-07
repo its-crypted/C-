951307d3f93c96202fe5f20b7d0ffd4da50c13a1
Piyush Thange <pthange19@gmail.com>
2021-04-07 22:48:35 +0530

    Adding basic arithmetic program
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/basic_arithmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 8 - 4;
+	cout << x << endl;
+	int a = 8 * 4;
+	cout << a << endl;
+	int b = 8 / 4;
+	cout << b << "\n";
+	int c = 8 % 4; //remainder
+	cout << c << endl;
+	//order of precedence is PEMDAS
+	return 0;
+}
