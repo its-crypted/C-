01b0e0d108e57c689fda52e2d7ceee1b3e11f869
Piyush Thange <pthange19@gmail.com>
2021-04-08 10:58:24 +0530

    Adding function basics
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void printsome(); //function prototype
+
+int main()
+{
+	printsome();
+	return 0;
+}
+
+void printsome(){
+	cout << "Here I'm a function" << endl;
+}
