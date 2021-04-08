1ef2be603506e51e9f97a2b52c66bb839163d866
Piyush Thange <pthange19@gmail.com>
2021-04-08 10:25:04 +0530

    Adding if else
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/if_else.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+
+//Relational >=, <=, >, <
+//Quality operators ==, !=
+int main()
+{
+	if(1 > 3) {		
+		cout << "Awesome";
+	}
+	else {
+		cout << "Nosome";
+	}
+	return 0;
+}
