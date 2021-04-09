c043a1b09ac5477ea2e1eb90c9c3201b9a715853
Piyush Thange <pthange19@gmail.com>
2021-04-09 20:57:58 +0530

    Changing the main to hello.cpp
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //this is a preprocesso directive
+
+using namespace std;	//std - standard library.
+
+int main()//A function below this. int is function type. main is func name
+{
+	cout << "Hello, World!" <<endl;	//These are statements & made to terminate by ;
+	return 0; //main ran fine.	
+}
