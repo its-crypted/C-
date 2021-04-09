3f94365ea188b2a302ecd3f72b484d94c2f5a44d
Piyush Thange <pthange19@gmail.com>
2021-04-10 00:01:32 +0530

    Adding if else
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/if_else.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 10;
+
+	if (x == 10) {
+		cout << "It's equal" << endl;
+	}
+	else if(x < 87) {
+		cout << "It's less than 87" << endl;
+	}
+	else if(x > 87) {
+		cout << "Greater than 87" <<endl;
+	}
+	else {
+		cout << "not equal" << endl;
+
+	return 0;
+}
