36571cf06c02165aba6a34204327ad786b24f10b
Piyush Thange <pthange19@gmail.com>
2021-04-10 13:39:18 +0530

    Adding the do while loop which will test the code first and then the while condition
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/do_while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//This will test the code first and then check the condition for while
+	int x = 1;
+	do{
+		cout << x << endl;
+		x++;
+	} while(x < 10);
+	return 0;
+}
