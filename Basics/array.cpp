63cdcc1a1dc4ee48ba4623953b872ad612419e26
Piyush Thange <pthange19@gmail.com>
2021-04-10 19:36:39 +0530

    Adding arrays.
    Array defining below.
    data_type array_name [length_of_arrary] = {elements seperated by commas}
    
    Array accessing.
    using array_name[index_of_element]
    
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+//array is a varibale containing mulitple values of same data type.
+
+int main()
+{
+	int pi[5] = {1,3,4,66,99};// {elements}
+	cout << pi[1] << endl;
+	return 0;
+}
