816f97c4ed6964058e53ed289a111dade4ae7708
Piyush Thange <pthange19@gmail.com>
2021-04-11 15:25:23 +0530

    size of function
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/size_of.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// sizeof() function dietermines the sizeof an
+//array,variable, or constant. in bytes. 
+
+using namespace std;
+
+int main()
+{
+	//int c;
+	double pi[10];
+	cout << sizeof(pi) << endl;
+	return 0;
+}
