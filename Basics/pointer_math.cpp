7dbe8eaab5fdd09b54874457253c60caabaa6c3c
Piyush Thange <pthange19@gmail.com>
2021-04-11 15:45:14 +0530

    Pointer & its Math.
    *ptr = &var_name . This will store the address of var_name in variable ptr.
    we can retrieve the value of a pointer using the *ptr
    
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/pointer_math.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int bucky[5] = {44, 29, 235, 586, 8845};
+	int *bp0 = &bucky[0];
+	int *bp1 = &bucky[1];
+	int *bp2 = &bucky[2];
+	int *bp3 = &bucky[3];
+	int *bp4 = &bucky[4];
+
+	
+	cout << "bp0 is at " << bp0 << endl;
+	cout << "bp1 is at " << bp1 << endl;
+	cout << "bp2 is at " << bp2 << endl;
+	cout << "bp3 is at " << bp3 << endl;
+	cout << "bp4 is at " << bp4 << endl;
+	
+	bp0 += 2;
+	cout << "bp0 is now at " << bp0 << endl;
+	cout << "value of bp0 is " << *bp0 << endl;
+	return 0;
+}
