324a6b8ffd3548e1de07376d9d6a60ccdc0034bd
Piyush Thange <pthange19@gmail.com>
2021-04-17 13:14:21 +0530

    Adding a repo for protected class
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/protected/inheritance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "mother.h"
+#include "daughter.h"
+
+using namespace std;
+
+int main()
+{
+	Mother mom;
+	mom.sayName();
+
+	Daughter tina;
+	tina.sayName();
+	return 0;
+}
