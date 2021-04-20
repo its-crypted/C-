a0e26c1b1dc61e868eca486bcd7591501548f809
Piyush Thange <pthange19@gmail.com>
2021-04-20 22:50:56 +0530

    Virtual Function
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Basics/virtual_function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//Virtual funtion are template function which can provide same function facility
+using namespace std;
+
+class Enemy {
+	public:
+		virtual void attack(){}
+};
+
+class Ninja: public Enemy{
+	public:
+		void attack(){
+			cout << "ninja attack!" << endl;
+		}
+};
+
+class Monster: public Enemy{
+	public:
+		void attack(){
+			cout << "MONSTER attack!!" << endl;
+		}
+};
+
+
+int main(){
+	Ninja n;
+	Monster m;
+	Enemy *enemy1 = &n;
+	Enemy *enemy2 = &m;
+	enemy1->attack();
+	enemy2->attack();
+	return 0;
+}
